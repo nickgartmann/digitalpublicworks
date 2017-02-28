f769e14da0e133d0fa2d57aeb7799f2ac8f852eb
Nick Gartmann <nick.gartmann@gmail.com>
2017-02-28 10:49:37 -0600

    prod config for repo

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,11 @@ config :dpw, DPW.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
+config :dpw, DPW.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20,
+  ssl: true
 # Do not print debug messages in production
 config :logger, level: :info
 
